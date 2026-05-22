b10847852298448287834e1b8e6db321e2f9f402
i9830162750-sudo <i9830162750@gmail.com>
2026-05-23 01:29:47 +0530

    Add new route for Saavn search and stream proxy

--- api/routes/youtube.js
+++ api/routes/youtube.js
@@ -4,9 +4,10 @@ const express    = require('express');
 const router     = express.Router();
 const controller = require('../controllers/youtube');
 
-router.get('/search',   controller.search);
-router.get('/videos',   controller.videoDetails);
-router.get('/playlist', controller.playlistItems);
-router.get('/stream',   controller.streamProxy);   // ← ADD THIS
+router.get('/search',        controller.search);
+router.get('/videos',        controller.videoDetails);
+router.get('/playlist',      controller.playlistItems);
+router.get('/stream',        controller.streamProxy);
+router.get('/saavn/search',  controller.saavnSearch);
 
 module.exports = router;
