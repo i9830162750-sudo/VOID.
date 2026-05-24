6a8198b89e5181bd9e06c110aa9aec91130a6995
i9830162750-sudo <i9830162750@gmail.com>
2026-05-24 12:31:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,7 @@ app.use(
       directives: {
         defaultSrc:  ["'self'"],
         scriptSrc:     ["'self'", "'unsafe-inline'", 'https://cdn.jsdelivr.net', 'https://cdnjs.cloudflare.com'],
+        scriptSrcAttr: ["'unsafe-inline'"],
         styleSrc:    ["'self'", "'unsafe-inline'", 'https://fonts.googleapis.com'],
         fontSrc:     ["'self'", 'https://fonts.gstatic.com'],
         imgSrc:      ["'self'", 'data:', 'https://*.ytimg.com', 'https://*.ggpht.com'],
